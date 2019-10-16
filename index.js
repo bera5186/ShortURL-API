6dfe3482e3e5a9756b0c783a5510373ba41927f9
bera5186 <rbasu611@gmail.com>
2019-10-16 23:53:55 +0530

    Created basic structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const connectDB = require('./config/db')
+
+const app = express()
+const PORT = 5000
+
+app.use(express.json({ extended : false }))
+
+// connect to database
+connectDB()
+
+// routes
+app.use('/', require('./routes/index'))
+app.use('/api/url', require('./routes/url'))
+
+app.listen(PORT , () => console.log(`Server running at ${PORT}`))
---
+++ routes/url.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const config = require('config')
+const validUrl = require('valid-url')
+const shortid = require('shortid')
+
+const model = require('../models/Url')
+
+// @route POST /api/url/shorten
+router.post('/shorten', async (req, res) => {
+    const { longUrl } = req.body
+    const baseUrl = config.get('baseUrl')
+
+    if(!validUrl.isUri(longUrl)) {
+        return res.statusCode(401).json('invalid base url 🤞')
+    }
+
+    // Create url codee
+    const urlCode = shortid.generate()
+
+    //
+    if (validUrl.isUri(longUrl)) {
+        try {
+            let url = await model.findOne({ 'longUrl' : longUrl })
+
+            if (url) {
+                res.json(url)
+            } else {
+                const shortUrl = baseUrl + '/' + urlCode
+                url = new model({
+                    longUrl,
+                    shortUrl,
+                    urlCode,
+                    date: new Date()
+                })
+
+                await url.save()
+                res.json(url)
+            }
+        } catch (error) {
+            console.error(error)
+            res.statusCode(500).json('Server Error 😥')
+        }
+    } else {
+        res.statusCode(401).json('Invalid Base Url 🤞')
+    }
+})
+
+
+module.exports = router
