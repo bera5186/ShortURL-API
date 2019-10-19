6fe150c99cd62892c58b85458f91b5c391231932
bera5186 <rbasu611@gmail.com>
2019-10-19 14:41:06 +0530

    Ready to deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const connectDB = require('./config/db')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({ extended : false }))
 
