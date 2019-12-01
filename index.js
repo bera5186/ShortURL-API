77e800af7a1cfbd42d784c2e6b6b63b5083f3b65
bera5186 <rbasu611@gmail.com>
2019-12-01 21:50:58 +0530

    Production Bug Fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({ extended : false }))
 app.set("views", path.join(__dirname, "views"))
